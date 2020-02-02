4cb8f5a61a9238b7fbc794ff95e3cba6cbaf62ae
bob <ismail.amim2010@gmail.com>
2020-02-02 11:52:00 +0100

    add compression lib for gzipping on heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors       = require('cors');
 const bodyParser = require('body-parser');
 // path is native  node module 
 const path       = require('path'); 
+const compression= require('compression');
 
 if(process.env.NODE_ENV!=='production')
 //get env key 
@@ -13,8 +14,9 @@ const app =express();
 const port = process.env.PORT || 5000;
 
 // find body and convert to json
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use( compression() );
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({extended: true}) );
 
 // able to properly make request 
 // cors make sure that we are from the same web server
